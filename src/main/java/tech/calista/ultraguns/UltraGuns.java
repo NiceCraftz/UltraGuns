426e8689d5dc74ac4192d3b306f843efaaade879
Alessandro <alessandromaria.calista@gmail.com>
2022-08-30 00:15:04 +0200

    Initial commit

---
+++ src/main/java/tech/calista/ultraguns/UltraGuns.java
@@ -0,0 +1,17 @@
+package tech.calista.ultraguns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UltraGuns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
